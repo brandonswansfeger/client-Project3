51be8cd5192e6dcf6cecd287ade57f8fdcc8feca
brandonswansfeger <88030396+brandonswansfeger@users.noreply.github.com>
2021-12-07 23:40:14 -0500

    logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ function Header(props) {
     h1 > AddMe - QRC < /h1> <
     /Link> <
     h2 >. . . . create your own personal QR Code< /h2>
-    <img src="https://i.ibb.co/XxWRNvC/Picture7.png" alt="alternatetext"  width="100" height="100"></img>
+    <img src="https://i.ibb.co/ydKxNzT/hand-rtrabsparent.png" alt="alternatetext"  width="100" height="100"></img>
     <
     /nav>
   );
